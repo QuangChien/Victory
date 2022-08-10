5a59eb5bd169d14ac4624a0a12e9fec619bd920b
quangchien <quangchienxt2001@gmail.com>
2022-08-10 22:24:25 +0700

    10/08/2022: Create module, route, blog category table

---
+++ Blog/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author  QuangChien(Glorious Victory) <quangchien01.it@gmail.com>
+ * @copyright Copyright © 2022 QuangChien(Glorious Victory) <https://www.facebook.com/quangchien01>. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Victory_Blog',
+    __DIR__
+);
